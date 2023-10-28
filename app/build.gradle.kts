13cca742bac55ebc4a4cc469e1f562cc6f8c07bb
MALIKFAZAL1 <malikfazal268@gmail.com>
2023-10-27 21:29:12 -0400

    malik inventory commit 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation("com.google.firebase:firebase-auth:22.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -44,5 +45,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.firebase:firebase-client-android:2.5.2")
+    implementation("com.google.gms:google-services:4.3.2")
+    implementation("com.google.firebase:firebase-core:9.6.1")
     //End
 }
