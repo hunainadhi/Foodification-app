ce1aa4f6a100f06b7f19d52251a7960741236da0
MALIKFAZAL1 <malikfazal268@gmail.com>
2023-11-23 22:00:22 -0500

    Malik Inventory FINAL

--- app/src/main/java/com/example/foodification/IngredientAdapter.java
+++ app/src/main/java/com/example/foodification/IngredientAdapter.java
@@ -4,28 +4,90 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.database.DataSnapshot;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.IngredientViewHolder> {
+public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.ViewHolder> {
 
-    private Context context;
     private List<Ingredient> ingredients;
-    private OnItemClickListener itemClickListener;
-    private int selectedItemPosition = -1;
+    private LayoutInflater inflater;
+    private OnItemClickListener listener;
 
-    public IngredientAdapter(Context context) {
-        this.context = context;
-        this.ingredients = new ArrayList<>();
+    public IngredientAdapter(Context context, List<Ingredient> ingredients, OnItemClickListener listener) {
+        this.inflater = LayoutInflater.from(context);
+        this.ingredients = ingredients;
+        this.listener = listener;
     }
 
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.item_ingredient, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Ingredient ingredient = ingredients.get(position);
+        holder.bind(ingredient, listener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return ingredients.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView nameTextView;
+        private TextView quantityTextView;
+        private TextView unitTextView;
+        private Button deleteButton;
+        private Button modifyButton;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameTextView = itemView.findViewById(R.id.nameTextView);
+            quantityTextView = itemView.findViewById(R.id.quantityTextView);
+            unitTextView = itemView.findViewById(R.id.unitTextView);
+            deleteButton = itemView.findViewById(R.id.deleteButton);
+            modifyButton = itemView.findViewById(R.id.modifyButton);
+        }
+
+        public void bind(final Ingredient ingredient, final OnItemClickListener listener) {
+            nameTextView.setText(ingredient.getName());
+            quantityTextView.setText(String.valueOf(ingredient.getQuantity()));
+            unitTextView.setText(ingredient.getUnit());
+
+            deleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    listener.onDeleteClick(getAdapterPosition());
+                }
+            });
+
+            modifyButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    listener.onModifyClick(getAdapterPosition());
+                }
+            });
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onDeleteClick(int position);
+        void onModifyClick(int position);
+    }
+
+    // Setter method to update the ingredients list
     public void setIngredients(Iterable<DataSnapshot> ingredientSnapshots) {
         ingredients.clear();
         for (DataSnapshot snapshot : ingredientSnapshots) {
@@ -34,89 +96,11 @@ public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.In
         }
         notifyDataSetChanged();
     }
-
     public void clearIngredients() {
         ingredients.clear();
         notifyDataSetChanged();
     }
-    public int getSelectedItemPosition() {
-        return selectedItemPosition;
-    }
-
     public Ingredient getIngredient(int position) {
         return ingredients.get(position);
-    }
-
-    public void clearSelectedItem() {
-        selectedItemPosition = -1;
-        notifyDataSetChanged();
-    }
-
-    public void setOnItemClickListener(OnItemClickListener listener) {
-        this.itemClickListener = listener;
-    }
-
-    @NonNull
-    @Override
-    public IngredientViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item_ingredient, parent, false);
-        return new IngredientViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull IngredientViewHolder holder, int position) {
-        Ingredient ingredient = ingredients.get(position);
-        holder.nameTextView.setText(ingredient.getName());
-        holder.quantityTextView.setText(String.valueOf(ingredient.getQuantity()));
-        holder.unitTextView.setText(ingredient.getUnit());
-
-        // Highlight the selected item
-        if (selectedItemPosition == position) {
-            holder.itemView.setBackgroundColor(ContextCompat.getColor(context, R.color.selected_item_color));
-        } else {
-            holder.itemView.setBackgroundColor(ContextCompat.getColor(context, android.R.color.transparent));
-        }
-
-        // Set a click listener for the item
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int clickedPosition = holder.getAdapterPosition();
-                setSelectedItemPosition(clickedPosition);
-
-                if (itemClickListener != null) {
-                    itemClickListener.onItemClick(clickedPosition);
-                }
-            }
-        });
-    }
-
-    @Override
-    public int getItemCount() {
-        return ingredients.size();
-    }
-
-    public class IngredientViewHolder extends RecyclerView.ViewHolder {
-        TextView nameTextView;
-        TextView quantityTextView;
-        TextView unitTextView;
-
-        public IngredientViewHolder(View itemView) {
-            super(itemView);
-            nameTextView = itemView.findViewById(R.id.nameTextView);
-            quantityTextView = itemView.findViewById(R.id.quantityTextView);
-            unitTextView = itemView.findViewById(R.id.unitTextView);
-        }
-    }
-
-    // Method to set the selected item position
-    public void setSelectedItemPosition(int position) {
-        selectedItemPosition = position;
-        notifyDataSetChanged();
-    }
-
-    // Interface for item click callbacks
-    public interface OnItemClickListener {
-        void onItemClick(int position);
     }
 }
--- app/src/main/java/com/example/foodification/Inventory.java
+++ app/src/main/java/com/example/foodification/Inventory.java
@@ -2,24 +2,21 @@ package com.example.foodification;
 
 import static android.content.ContentValues.TAG;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import android.view.ContextMenu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -32,27 +29,24 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class Inventory extends AppCompatActivity {
 
     private DatabaseReference databaseReference;
     private RecyclerView ingredientList;
     private String email, safeEmail;
     private IngredientAdapter adapter;
-    private static final int MENU_DELETE_OPTION = R.id.delete_option;
-    private static final int MENU_MODIFY_OPTION = R.id.modify_option;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
-        //email = getIntent().getStringExtra("USER_EMAIL");
 
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         String email = preferences.getString("global_variable_key", "default_value");
 
-
         FirebaseApp.initializeApp(this);
-
 
         safeEmail = email.replace('.', ',')
                 .replace('#', '-')
@@ -68,16 +62,31 @@ public class Inventory extends AppCompatActivity {
         ingredientList = findViewById(R.id.ingredientList);
         Button backbtn = findViewById(R.id.backButton);
 
-        // Initialize RecyclerView and Adapter
-        adapter = new IngredientAdapter(this);
+        adapter = new IngredientAdapter(this, new ArrayList<Ingredient>(), new IngredientAdapter.OnItemClickListener() {
+            @Override
+            public void onDeleteClick(final int position) {
+                // Handle the delete action here
+                showDeleteConfirmationDialog(position);
+            }
+
+            @Override
+            public void onModifyClick(int position) {
+                // Handle the modify action here
+                Ingredient ingredient = adapter.getIngredient(position);
+                if (ingredient != null) {
+                    showModifyDialog(ingredient);
+                }
+            }
+        });
+
         ingredientList.setAdapter(adapter);
         ingredientList.setLayoutManager(new LinearLayoutManager(this));
 
         updateInventory();
+
         addIngredientButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Show a dialog to add ingredients
                 showAddIngredientDialog();
             }
         });
@@ -85,11 +94,10 @@ public class Inventory extends AppCompatActivity {
         backbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              finish();
+                finish();
             }
         });
 
-        // Attach a listener to retrieve data
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -98,100 +106,60 @@ public class Inventory extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                // Handle errors
                 Log.w(TAG, "loadPost:onCancelled", error.toException());
             }
         });
+    }
 
-        // Register the RecyclerView for a context menu
-        registerForContextMenu(ingredientList);
+    private void showDeleteConfirmationDialog(final int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Delete Ingredient");
+        builder.setMessage("Are you sure you want to delete this ingredient?");
 
-        // Set an item click listener for the RecyclerView
-// Set an item click listener for the RecyclerView
-        adapter.setOnItemClickListener(new IngredientAdapter.OnItemClickListener() {
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
             @Override
-            public void onItemClick(int position) {
-                // Handle item click
-                Ingredient ingredient = adapter.getIngredient(position);
-                if (ingredient != null) {
-                    // Display details for the clicked ingredient
-                    showIngredientDetails(ingredient);
-                }
+            public void onClick(DialogInterface dialogInterface, int i) {
+                deleteIngredient(position);
             }
         });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // Cancel the deletion
+            }
+        });
+
+        builder.show();
     }
 
-    // Method to show the context menu when an item is long-pressed
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        getMenuInflater().inflate(R.menu.context_menu, menu);
-    }
-
-    // Handle the context menu item selection
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        int selectedItemPosition = adapter.getSelectedItemPosition();
-
-        if (selectedItemPosition == -1) {
-            return super.onContextItemSelected(item);
-        }
-
-        Ingredient selectedIngredient = adapter.getIngredient(selectedItemPosition);
-
-        if (item.getItemId() == R.id.delete_option) {
-            // Delete the selected ingredient
-            deleteIngredient(selectedIngredient);
-            return true;
-        } else if (item.getItemId() == R.id.modify_option) {
-            // Show a modify dialog and update the selected ingredient
-            showModifyDialog(selectedIngredient);
-            return true;
-        }
-
-        return super.onContextItemSelected(item);
-    }
-
-
-
-    // Method to delete an ingredient from Firebase
-    private void deleteIngredient(Ingredient ingredient) {
+    private void deleteIngredient(int position) {
+        Ingredient ingredient = adapter.getIngredient(position);
         if (ingredient != null) {
             databaseReference.child(ingredient.getId()).removeValue();
             Toast.makeText(this, "Ingredient deleted!", Toast.LENGTH_SHORT).show();
-            adapter.clearSelectedItem();
         }
     }
-    // Method to show a dialog for modifying ingredients
+
     private void showModifyDialog(final Ingredient ingredient) {
         final Dialog dialog = new Dialog(this);
         dialog.setContentView(R.layout.dialog_add_ingredient);
 
         final EditText nameEditText = dialog.findViewById(R.id.nameEditText);
         final EditText quantityEditText = dialog.findViewById(R.id.quantityEditText);
-        //final EditText unitEditText = dialog.findViewById(R.id.unitEditText);
-//        String[] units = new String[] {"kg", "g", "lbs", "oz", "ml", "l", "cup","tsp","tbsp"}; // Example units
-//
-//        AutoCompleteTextView unitAutoCompleteTextView = findViewById(R.id.unitAutoCompleteTextView);
-//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, units);
-//        unitAutoCompleteTextView.setAdapter(adapter);
-
-        String[] units = new String[] {"Select Unit", "kg", "g", "lbs", "oz", "ml", "l", "cup"}; // Add "Select Unit" as the first item
-        Spinner unitSpinner = findViewById(R.id.unitSpinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, units);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        unitSpinner.setAdapter(adapter);
-
+        final Spinner unitSpinner = dialog.findViewById(R.id.unitSpinner);
         Button modifyButton = dialog.findViewById(R.id.addButton);
 
         nameEditText.setText(ingredient.getName());
         quantityEditText.setText(String.valueOf(ingredient.getQuantity()));
-        //unitEditText.setText(ingredient.getUnit());
 
-        //unitAutoCompleteTextView.setText(ingredient.getUnit());
-        int spinnerPosition = adapter.getPosition(ingredient.getUnit());
+        String[] units = new String[] {"Select Unit", "kg", "g", "lbs", "oz", "ml", "l", "cup"};
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, units);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        unitSpinner.setAdapter(spinnerAdapter);
+
+        int spinnerPosition = spinnerAdapter.getPosition(ingredient.getUnit());
         unitSpinner.setSelection(spinnerPosition);
-
 
         modifyButton.setText("Modify");
 
@@ -201,16 +169,14 @@ public class Inventory extends AppCompatActivity {
                 String name = nameEditText.getText().toString().trim();
                 String quantityStr = quantityEditText.getText().toString().trim();
                 String unit = unitSpinner.getSelectedItem().toString().trim();
-//String selectedUnit = unitSpinner.getSelectedItem().toString();
-                if (!name.isEmpty() && !quantityStr.isEmpty() && !unit.isEmpty()) {
+
+                if (!name.isEmpty() && !quantityStr.isEmpty() && !unit.isEmpty() && !unit.equalsIgnoreCase("Select Unit")) {
                     double quantity = Double.parseDouble(quantityStr);
 
-                    // Update the selected ingredient's details
                     ingredient.setName(name);
                     ingredient.setQuantity(quantity);
                     ingredient.setUnit(unit);
 
-                    // Update the ingredient in Firebase
                     databaseReference.child(ingredient.getId()).setValue(ingredient);
 
                     Toast.makeText(Inventory.this, "Ingredient modified!", Toast.LENGTH_SHORT).show();
@@ -224,22 +190,19 @@ public class Inventory extends AppCompatActivity {
         dialog.show();
     }
 
-    // Method to show the dialog for adding ingredients
     private void showAddIngredientDialog() {
-        // Your add ingredient dialog code here
         final Dialog dialog = new Dialog(this);
         dialog.setContentView(R.layout.dialog_add_ingredient);
 
         final EditText nameEditText = dialog.findViewById(R.id.nameEditText);
         final EditText quantityEditText = dialog.findViewById(R.id.quantityEditText);
-        //final EditText unitEditText = dialog.findViewById(R.id.unitEditText);
-        String[] units = new String[] {"Select Unit", "Kg", "g", "Lbs", "Oz", "Ml", "L", "Cup"}; // Add "Select Unit" as the first item
         final Spinner unitSpinner = dialog.findViewById(R.id.unitSpinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, units);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        unitSpinner.setAdapter(adapter);
-
         Button addButton = dialog.findViewById(R.id.addButton);
+
+        String[] units = new String[] {"Select Unit", "Kg", "g", "Lbs", "Oz", "Ml", "L", "Cup"};
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, units);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        unitSpinner.setAdapter(spinnerAdapter);
 
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -248,14 +211,12 @@ public class Inventory extends AppCompatActivity {
                 String quantityStr = quantityEditText.getText().toString().trim();
                 String unit = unitSpinner.getSelectedItem().toString().trim();
 
-                if (!name.isEmpty() && !quantityStr.isEmpty() && !unit.isEmpty()||unit.equalsIgnoreCase("Select Unit")) {
+                if (!name.isEmpty() && !quantityStr.isEmpty() && !unit.isEmpty() && !unit.equalsIgnoreCase("Select Unit")) {
                     double quantity = Double.parseDouble(quantityStr);
 
-                    // Create a new Ingredient object
                     String ingredientId = databaseReference.push().getKey();
                     Ingredient ingredient = new Ingredient(ingredientId, name, quantity, unit);
 
-                    // Save the ingredient to Firebase
                     if (ingredientId != null) {
                         databaseReference.child(ingredientId).setValue(ingredient);
                         Toast.makeText(Inventory.this, "Ingredient added!", Toast.LENGTH_SHORT).show();
@@ -268,8 +229,8 @@ public class Inventory extends AppCompatActivity {
         });
 
         dialog.show();
-
     }
+
     private void updateInventory() {
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -287,27 +248,4 @@ public class Inventory extends AppCompatActivity {
             }
         });
     }
-    private void showIngredientDetails(Ingredient ingredient) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Ingredient Details");
-
-        // Customize the dialog content based on the ingredient
-        String details = "Name: " + ingredient.getName() + "\n" +
-                "Quantity: " + ingredient.getQuantity() + " " + ingredient.getUnit();
-
-        builder.setMessage(details);
-
-        // Add a button to close the dialog
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-
-        builder.show();
-    }
-
-
-
-
 }
