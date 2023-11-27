e32b243dd0e0b6e7eb64494d0ce83bb6aa4615aa
abmn12 <muhayminstar@gmail.com>
2023-11-27 17:43:34 -0500

    Updated Account Screen and added ScrollView to Home Screen

--- app/src/main/java/com/example/foodification/UserInfoFragment.java
+++ app/src/main/java/com/example/foodification/UserInfoFragment.java
@@ -6,88 +6,34 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class UserInfoFragment extends Fragment {
 
     private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+    TextView btnSignOut;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_user_info, container, false);
 
         // Find buttons by their IDs
-        Button btnSettings = view.findViewById(R.id.btnSettings);
-        Button btnEditProfile = view.findViewById(R.id.btnEditProfile);
-        Button btnFavorites = view.findViewById(R.id.btnFavorites);
-        Button btnAccountInfo = view.findViewById(R.id.btnAccountInfo);
-        Button btnHelp = view.findViewById(R.id.btnHelp);
-        Button btnSignOut = view.findViewById(R.id.btnSignOut);
-
-        // Set click listeners for each button
-        btnSettings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Handle Settings button click
-                // Example: Open SettingsFragment
-                // getActivity().getSupportFragmentManager().beginTransaction()
-                //     .replace(R.id.fragmentContainer, new SettingsFragment())
-                //     .addToBackStack(null)
-                //     .commit();
-            }
-        });
-
-        btnEditProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Handle Edit Profile button click
-                // Example: Open EditProfileFragment
-                // getActivity().getSupportFragmentManager().beginTransaction()
-                //     .replace(R.id.fragmentContainer, new EditProfileFragment())
-                //     .addToBackStack(null)
-                //     .commit();
-            }
-        });
-
-        btnFavorites.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Handle Favorites button click
-                // Example: Open FavoritesFragment
-                // getActivity().getSupportFragmentManager().beginTransaction()
-                //     .replace(R.id.fragmentContainer, new FavoritesFragment())
-                //     .addToBackStack(null)
-                //     .commit();
-            }
-        });
-
-        btnAccountInfo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Handle Account Information button click
-                // Example: Open AccountInfoFragment
-                // getActivity().getSupportFragmentManager().beginTransaction()
-                //     .replace(R.id.fragmentContainer, new AccountInfoFragment())
-                //     .addToBackStack(null)
-                //     .commit();
-            }
-        });
-
-        btnHelp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Handle Help button click
-                // Example: Open HelpFragment
-                // getActivity().getSupportFragmentManager().beginTransaction()
-                //     .replace(R.id.fragmentContainer, new HelpFragment())
-                //     .addToBackStack(null)
-                //     .commit();
-            }
-        });
-
+        btnSignOut = view.findViewById(R.id.btnSignOut);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        fetchAndDisplayUserName(view);
         btnSignOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -101,15 +47,45 @@ public class UserInfoFragment extends Fragment {
     }
 
     private void signOutUser() {
+
+        mAuth.signOut();
         Intent intent = new Intent(requireActivity(), FirstActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
 
-        mAuth.signOut();
-
         // Redirect back to the sign-in activity (Assuming SignInFragment is your sign-in fragment)
         // Finish the current activity to prevent going back to it
         requireActivity().finish();
+    }
+    private void fetchAndDisplayUserName(View view) {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+
+            DatabaseReference userRef = mDatabase.child("users").child(userId);
+
+            userRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        // Assuming you have a "name" field in your user node
+                        String userName = dataSnapshot.child("name").getValue(String.class);
+
+                        // Now you have the user's name, you can use it as needed
+                        // For example, display it in a TextView
+                        TextView userNameTextView = view.findViewById(R.id.nameText);
+                        userNameTextView.setText(userName);
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    // Handle errors
+                    Toast.makeText(getActivity(), "Failed to retrieve user data", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
 
